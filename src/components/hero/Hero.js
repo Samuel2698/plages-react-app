3c97cd6ba819d32967c9e89593d5b4bad44ef35f
Samuel Velasco <samuelvelasco2698@gmail.com>
2022-06-30 17:30:22 +0200

    Add files via upload

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,30 +1,33 @@
-import React from 'react'
-import './HeroStyles.css'
-import {AiOutlineSearch} from 'react-icons/ai'
+import React from "react";
+import "./HeroStyles.css";
+import { AiOutlineSearch } from "react-icons/ai";
 
-import Video from '../../assets/maldivesVideo.mp4'
+import Video from "../../assets/maldivesVideo.mp4";
 
 const Hero = () => {
   return (
-    <div className='hero'>
-        <video autoPlay loop muted id='video'>
-            <source src={Video} type='video/mp4' />
-        </video>
-        <div className='overlay'></div>
-        <div className='content'>
-          <h1>Ton voyage de rêve!</h1>
-          <h2>Le TOP mondial des plus belles plages</h2>
-          <form className='form'>
-            <div>
-              <input type="text" placeholder='Trouver une destination'/>
-            </div>
-            <div>
-              <button> <AiOutlineSearch className='icon'/></button>
-            </div>
-          </form>
-        </div>
+    <div className="hero">
+      <video autoPlay loop muted id="video">
+        <source src={Video} type="video/mp4" />
+      </video>
+      <div className="overlay"></div>
+      <div className="content">
+        <h1>Ton voyage de rêve!</h1>
+        <h2>Le TOP mondial des plus belles plages</h2>
+        <form className="form">
+          <div>
+            <input type="text" placeholder="Trouver une destination" />
+          </div>
+          <div>
+            <button>
+              {" "}
+              <AiOutlineSearch className="icon" />
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
